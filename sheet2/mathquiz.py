f20bfb08da009893bf45408610f38843399fcf5c
Kirubel Belay <KirubelB4697@gmail.com>
2023-07-05 08:04:48 -0700

    Add mathquiz

---
+++ sheet2/mathquiz.py
@@ -0,0 +1,48 @@
+print("Are you ready to get started")
+num1 = 2
+num2 = 7
+question = "what is 'num1' * num2? "
+if 1<2:
+    print('something is wrong!')
+else:
+    print("something is right!")
+if 1 > 1000:
+    print("ouch!")
+print("boink!")
+import random
+number = int(input("Enter a number: "))
+    print("the number is bigger than 100")
+else:
+    print("the number is not bigger than 100!")
+
+number = int(input("Enter a number: "))
+if number == 100:
+    print("the number is equal to 100!")
+else:
+    print("the number is not equal to 100")
+
+import random
+num1 = 2
+num2 = 15
+question = f"what is (num1) times (num2)? "
+user_answer = int(input(question))
+"correct_answer" == "30"
+if user_answer == "correct_answer":
+    print("Yes!")
+else:
+    print("No!")
+for x in 10, 20, 30, 40, 50, 60:
+    n = int(input("please give me a number: "))
+
+    if n==x:
+        print("you got it! your number and mine are both " + str(x) + ".")
+    else:
+        print("your number, " + str(n) + " is not " + str(x) + ".")
+odd = 1
+for x in 1, 2, 3, 4, 5, 6,:
+    print('an odd number:' , odd)
+    odd = odd + 2
+right = 0
+right = right + 1
+print("I asked you 10 questions. You got " + str(right) + " of them right.")
+print("well done!")b

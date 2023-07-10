95ec05d7cb6f0f6b17abe2541feff5efece0119a
Kirubel Belay <KirubelB4697@gmail.com>
2023-07-09 22:04:02 -0700

    Add Pictures

--- sheet 3/pictures.py
+++ sheet 3/pictures.py
@@ -1,5 +1,39 @@
 from gasp import *
 begin_graphics()
-Line ((200, 100), (100,300))
-update_when('key_pressed')
+for r in 100, 110, 120, 130, 140:
+    Circle((300, 200), r) 
+    Circle((285,210), r)
+    Circle((315,210), r)
+    Arc((300,200), 30, 225, 315)
+update_when('key_press')
+end_graphics()
+def shout()
+    print('Python is wonderful!')
+    print('And so am I!')
+shout()
+def moan('')
+    ('Python is useless!')
+    ('And so are these worksheets.')
+def twice(x):
+     print(x, '+',x, 'is', x + x)
+     print(7, '+', 7, 'is', 7 + 7)
+def add(x, y):
+    print(x, '+', y, '=', x + y)
+def draw_face(x, y)
+     blah blah
+def draw_face(x, y)
+    ('first line')
+    ('second line')
+    ('third line')
+    ('etc')
+Circle((x-15, y+10), 5)
+Circle((x+15, y+10), 5)
+Line((x, y+10), (x-10, y-10))
+draw_face(300,200)
+draw_face(400,200)
+draw_face(350,270)
+from gasp import *
+begin_graphics()
+for i in range(0,400):
+    Line((i, 0), (0, 400-i))
 end_graphics
